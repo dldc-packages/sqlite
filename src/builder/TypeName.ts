7d5980131ecbe080bb0618bd89fa56b050ce8c25
Etienne Deladonchamps <e.deladonchamps@gmail.com>
2025-03-27 23:38:18 +0100

    fix: update ValidTypeName to use 'INT' instead of 'INTEGER'

--- src/builder/TypeName.ts
+++ src/builder/TypeName.ts
@@ -2,7 +2,7 @@ import * as n from "../Node.ts";
 
 type Node<K extends n.NodeKind = n.NodeKind> = n.Node<K>;
 
-export type ValidTypeName = "NULL" | "INTEGER" | "REAL" | "TEXT" | "BLOB";
+export type ValidTypeName = "NULL" | "INT" | "REAL" | "TEXT" | "BLOB";
 
 export function build(type: ValidTypeName): Node<"TypeName">;
 export function build(type: string): Node<"TypeName">;
